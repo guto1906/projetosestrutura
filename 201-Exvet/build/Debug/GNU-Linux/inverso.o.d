ef72b9dfb6fe7e51d0dc161a828c0c9b2a4e4c9e
test <test@teste.com>
2017-03-23 06:47:17 -0300

    Atualização 23mar

---
+++ 201-Exvet/build/Debug/GNU-Linux/inverso.o.d
@@ -0,0 +1,1 @@
+build/Debug/GNU-Linux/inverso.o: inverso.cpp
---
+++ 501_fibonacci/fibonacci.cpp
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   fibonacci.cpp
+ * Author: guto
+ *
+ * Created on 16 de Março de 2017, 20:56
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+void fibonacci(int i){
+    int vet[i-1];
+    vet[0] = 0;
+    vet[1] = 1;
+    vet[2] = 1;
+    
+    
+}
+
+
+int main(int argc, char** argv) {
+
+    int a;
+    cout << "Entre com um numero \n";
+    cin >> a;
+    fibonacci(a);
+}
+
